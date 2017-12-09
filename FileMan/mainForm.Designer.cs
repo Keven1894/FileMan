d516e2bcde265a21e4b90fd4fdd909a381fc3843
Peter Pinchao Liu <281538707@qq.com>
2017-12-09 14:02:55 -0500

    Init Check In

---
+++ FileMan/mainForm.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace FileMan
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Searchbutton = new System.Windows.Forms.Button();
+            this.fromDatePortionDateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.fromTimePortionDateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.toDatePorttionDateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.toTimePortionDateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.fromLabel = new System.Windows.Forms.Label();
+            this.toLabel = new System.Windows.Forms.Label();
+            this.SearchConditionsPanel = new System.Windows.Forms.Panel();
+            this.searchConditionsPanelLabel = new System.Windows.Forms.Label();
+            this.searchCueTextBox = new CueTextBox();
+            this.SuspendLayout();
+            // 
+            // Searchbutton
+            // 
+            this.Searchbutton.Location = new System.Drawing.Point(23, 173);
+            this.Searchbutton.Name = "Searchbutton";
+            this.Searchbutton.Size = new System.Drawing.Size(75, 23);
+            this.Searchbutton.TabIndex = 1;
+            this.Searchbutton.Text = "Search";
+            this.Searchbutton.UseVisualStyleBackColor = true;
+            this.Searchbutton.Click += new System.EventHandler(this.Searchbutton_Click);
+            // 
+            // fromDatePortionDateTimePicker
+            // 
+            this.fromDatePortionDateTimePicker.Location = new System.Drawing.Point(23, 64);
+            this.fromDatePortionDateTimePicker.Name = "fromDatePortionDateTimePicker";
+            this.fromDatePortionDateTimePicker.Size = new System.Drawing.Size(200, 20);
+            this.fromDatePortionDateTimePicker.TabIndex = 2;
+            // 
+            // fromTimePortionDateTimePicker
+            // 
+            this.fromTimePortionDateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.fromTimePortionDateTimePicker.Location = new System.Drawing.Point(247, 64);
+            this.fromTimePortionDateTimePicker.Name = "fromTimePortionDateTimePicker";
+            this.fromTimePortionDateTimePicker.ShowUpDown = true;
+            this.fromTimePortionDateTimePicker.Size = new System.Drawing.Size(101, 20);
+            this.fromTimePortionDateTimePicker.TabIndex = 3;
+            // 
+            // toDatePorttionDateTimePicker
+            // 
+            this.toDatePorttionDateTimePicker.Location = new System.Drawing.Point(23, 115);
+            this.toDatePorttionDateTimePicker.Name = "toDatePorttionDateTimePicker";
+            this.toDatePorttionDateTimePicker.Size = new System.Drawing.Size(200, 20);
+            this.toDatePorttionDateTimePicker.TabIndex = 4;
+            // 
+            // toTimePortionDateTimePicker
+            // 
+            this.toTimePortionDateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.toTimePortionDateTimePicker.Location = new System.Drawing.Point(247, 115);
+            this.toTimePortionDateTimePicker.Name = "toTimePortionDateTimePicker";
+            this.toTimePortionDateTimePicker.ShowUpDown = true;
+            this.toTimePortionDateTimePicker.Size = new System.Drawing.Size(101, 20);
+            this.toTimePortionDateTimePicker.TabIndex = 5;
+            // 
+            // fromLabel
+            // 
+            this.fromLabel.AutoSize = true;
+            this.fromLabel.Location = new System.Drawing.Point(23, 48);
+            this.fromLabel.Name = "fromLabel";
+            this.fromLabel.Size = new System.Drawing.Size(30, 13);
+            this.fromLabel.TabIndex = 6;
+            this.fromLabel.Text = "From";
+            // 
+            // toLabel
+            // 
+            this.toLabel.AutoSize = true;
+            this.toLabel.Location = new System.Drawing.Point(23, 99);
+            this.toLabel.Name = "toLabel";
+            this.toLabel.Size = new System.Drawing.Size(20, 13);
+            this.toLabel.TabIndex = 7;
+            this.toLabel.Text = "To";
+            // 
+            // SearchConditionsPanel
+            // 
+            this.SearchConditionsPanel.Location = new System.Drawing.Point(23, 228);
+            this.SearchConditionsPanel.Name = "SearchConditionsPanel";
+            this.SearchConditionsPanel.Size = new System.Drawing.Size(325, 283);
+            this.SearchConditionsPanel.TabIndex = 8;
+            // 
+            // searchConditionsPanelLabel
+            // 
+            this.searchConditionsPanelLabel.AutoSize = true;
+            this.searchConditionsPanelLabel.Location = new System.Drawing.Point(23, 209);
+            this.searchConditionsPanelLabel.Name = "searchConditionsPanelLabel";
+            this.searchConditionsPanelLabel.Size = new System.Drawing.Size(267, 13);
+            this.searchConditionsPanelLabel.TabIndex = 9;
+            this.searchConditionsPanelLabel.Text = "Search Conditions: (Click to Remove Search Condition)";
+            // 
+            // searchCueTextBox
+            // 
+            this.searchCueTextBox.Cue = "Search File";
+            this.searchCueTextBox.Location = new System.Drawing.Point(23, 12);
+            this.searchCueTextBox.Name = "searchCueTextBox";
+            this.searchCueTextBox.Size = new System.Drawing.Size(211, 20);
+            this.searchCueTextBox.TabIndex = 0;
+            this.searchCueTextBox.TextChanged += new System.EventHandler(this.cueTextBox1_TextChanged);
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1204, 523);
+            this.Controls.Add(this.searchConditionsPanelLabel);
+            this.Controls.Add(this.SearchConditionsPanel);
+            this.Controls.Add(this.toLabel);
+            this.Controls.Add(this.fromLabel);
+            this.Controls.Add(this.toTimePortionDateTimePicker);
+            this.Controls.Add(this.toDatePorttionDateTimePicker);
+            this.Controls.Add(this.fromTimePortionDateTimePicker);
+            this.Controls.Add(this.fromDatePortionDateTimePicker);
+            this.Controls.Add(this.Searchbutton);
+            this.Controls.Add(this.searchCueTextBox);
+            this.Name = "mainForm";
+            this.Text = "File Management Tool";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private CueTextBox searchCueTextBox;
+        private System.Windows.Forms.Button Searchbutton;
+        private System.Windows.Forms.DateTimePicker fromDatePortionDateTimePicker;
+        private System.Windows.Forms.DateTimePicker fromTimePortionDateTimePicker;
+        private System.Windows.Forms.DateTimePicker toDatePorttionDateTimePicker;
+        private System.Windows.Forms.DateTimePicker toTimePortionDateTimePicker;
+        private System.Windows.Forms.Label fromLabel;
+        private System.Windows.Forms.Label toLabel;
+        private System.Windows.Forms.Panel SearchConditionsPanel;
+        private System.Windows.Forms.Label searchConditionsPanelLabel;
+    }
+}
+
---
+++ FileMan/mainForm.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileMan
+{
+    public partial class mainForm : Form
+    {
+        public mainForm()
+        {
+            InitializeComponent();
+            SearchConditionsPanel.BorderStyle = BorderStyle.FixedSingle;
+        }
+
+        private void cueTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Searchbutton_Click(object sender, EventArgs e)
+        {
+            //to remove all RichTextBoxs
+            foreach (Control item in SearchConditionsPanel.Controls.OfType<RichTextBox>())
+            {
+                SearchConditionsPanel.Controls.Remove(item);
+            }
+            RichTextBox searchCueTextBoxRichTextBox = new RichTextBox
+            {
+                Text = "File name contains key word:" + searchCueTextBox.Text + "         X" 
+            };
+            searchCueTextBoxRichTextBox.Select(searchCueTextBoxRichTextBox.Text.Length, -1);
+            searchCueTextBoxRichTextBox.SelectionColor = Color.Red;
+            searchCueTextBoxRichTextBox.Enabled = false;
+            searchCueTextBoxRichTextBox.ReadOnly = true;
+            searchCueTextBoxRichTextBox.RightMargin = 0;
+            //searchCueTextBoxRichTextBox.Width = 60;
+            searchCueTextBoxRichTextBox.SelectAll();
+            searchCueTextBoxRichTextBox.SelectionAlignment = HorizontalAlignment.Right;
+            //set up the richtext height
+            using (Graphics g = CreateGraphics())
+            {
+                searchCueTextBoxRichTextBox.Height = (int)g.MeasureString(searchCueTextBoxRichTextBox.Text,
+                    searchCueTextBoxRichTextBox.Font, searchCueTextBoxRichTextBox.Width).Height + 7;
+            }
+            if (searchCueTextBox.Text != "")
+            {
+                SearchConditionsPanel.Controls.Add(searchCueTextBoxRichTextBox);
+            }
+        }
+    }
+}
